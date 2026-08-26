be033be44a78fbfc48adf76e7d65b0d3673dfe9e
Fernanda Conde <fercalaya07@gmail.com>
2026-08-26 16:50:05 -0600

    temporizador actualizado

--- Practica 3/Temporizador/Temporizador.c
+++ Practica 3/Temporizador/Temporizador.c
@@ -5,13 +5,14 @@
 
 #define ALARM_NUM 0 //define el numero de alarma
 #define ALARM_IRQ TIMER_IRQ_0 //define la interrupcion del timer
-#define INTERVAL_US 250000 //define el intervalo de tiempo en microsegundos
+#define INTERVAL_US 500000 //define el intervalo de tiempo en microsegundos
 #define TRACE_PIN 14 //define el gpio para la traza del osciloscopio
+#define LED_PIN 15 //define el pin del led externo
 volatile uint32_t next_alarm; //guarda el siguiente tiempo de alarma
 
-void on_alarm_irq() 
+void on_alarm_irq() {
     hw_clear_bits(&timer_hw->intr, 1u << ALARM_NUM); //limpia la interrupcion de la alarma
-    gpio_xor_mask(1u << PICO_DEFAULT_LED_PIN); //cambia el estado del led integrado
+    gpio_xor_mask(1u << LED_PIN); //cambia el estado del led externo
     gpio_xor_mask(1u << TRACE_PIN); //cambia el estado del gpio14
     next_alarm += INTERVAL_US; //calcula el siguiente tiempo de alarma
     timer_hw->alarm[ALARM_NUM] = next_alarm; //programa la siguiente alarma
@@ -19,8 +20,7 @@ void on_alarm_irq()
 
 int main() {
     stdio_init_all(); //inicia la comunicacion serial
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN; //define el led de la raspberry
-    gpio_init(LED_PIN); //inicia el pin del led
+    gpio_init(LED_PIN); //inicia el pin del led externo
     gpio_set_dir(LED_PIN, GPIO_OUT); //configura el led como salida
     gpio_put(LED_PIN, 0); //inicia el led apagado
     gpio_init(TRACE_PIN); //inicia el pin de la traza
@@ -29,8 +29,10 @@ int main() {
     irq_set_exclusive_handler(ALARM_IRQ, on_alarm_irq); //asigna la funcion a la interrupcion
     hw_set_bits(&timer_hw->inte, 1u << ALARM_NUM); //habilita la alarma del timer
     irq_set_enabled(ALARM_IRQ, true); //habilita la interrupcion en el nucleo
+
     next_alarm = timer_hw->timerawl + INTERVAL_US; //calcula el primer tiempo de alarma
     timer_hw->alarm[ALARM_NUM] = next_alarm; //programa la primera alarma
+
     while (true) {
         tight_loop_contents(); //mantiene el programa principal activo
     }
